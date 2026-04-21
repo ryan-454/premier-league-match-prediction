ea75d4370651cfd36a9ecaa6dcea7b35de8bdde3
Ryan George <ryan.george4504@gmail.com>
2026-04-21 12:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
